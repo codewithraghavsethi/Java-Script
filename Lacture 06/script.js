dfc58c84a3aa452192f54fa34a90daad850b89e7
codewithraghavsethi <codewithraghavsethi@gmail.com>
2024-10-06 21:57:05 +0530

    Add New Tutorial

---
+++ Lacture 06/script.js
@@ -0,0 +1,27 @@
+let id = document.getElementById("myHeading");
+console.log(id);
+console.dir(id);
+
+let clas = document.getElementsByClassName("myTopic");
+console.log(clas);
+console.dir(clas);
+
+let para = document.getElementsByTagName("p");
+console.log(para);
+console.dir(para); 
+
+let myId = document.querySelector("#myHeading");
+console.dir(myId);
+
+let myClass = document.querySelectorAll(".myClass");
+console.dir(myClass);
+
+let myTag = document.querySelectorAll("#myId");
+console.dir(myTag);
+
+
+ 
+
+
+
+
